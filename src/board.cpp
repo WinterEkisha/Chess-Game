6b3e22739992fee7deb144d995d55ebf045432ec
davidspiv <daspivack@gmail.com>
2025-04-22 23:42:18 -0700

    added makefile, moved code in src folder, added build folder to git ignore, fixed namespace errors, renamed Chess_game to not include a space

---
+++ src/board.cpp
@@ -0,0 +1,17 @@
+
+#include "board.h"
+
+#include <iostream>
+#include <string>
+
+#include "Piece_Classes.h"
+
+namespace Board {
+
+const int SIZE = 8;
+
+int board::boardInit() { return 2; }
+
+int board::boardPrint() { return 3; }
+
+};  // namespace Board
---
+++ src/board.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "Piece_Classes.h"
+
+namespace Board {
+
+class board {
+ public:
+  const int SIZE = 8;
+
+  int boardInit();   // initialized the board
+  int boardPrint();  // prints the current board
+
+  unsigned char locationToNumber();
+
+ private:
+  // TODO: maybe useful to store array here for each piece and square
+};
+
+}  // namespace Board
