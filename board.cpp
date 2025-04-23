274726e917e94b3c33502ce8aeadddea98fb8393
WinterEkisha <lukebourez@gmail.com>
2025-04-22 21:35:35 -0700

    first

---
+++ board.cpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "Piece_Classes.h"
+#include "board.h"
+
+
+const int SIZE = 8;
+
+int Board::boardInit(){
+	
+
+	return 2;
+}
+
+int Board::boardPrint() {
+
+	return 3;
+}
